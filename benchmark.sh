bf1b001f411431b40ae48b0b7c222a49675e7976
VÖRÖSKŐI András <voroskoi@gmail.com>
2022-05-03 22:52:33 +0200

    Use zig for benchmarking
    - drop hyperfine dependency
    - simplify benchmark.sh, drop bash dependency
    - merge gen_data.sh to benchmark.sh
    - create JSON output from run_bench.zig

--- benchmark.sh
+++ benchmark.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 set -e
 
 zig build bench -Drelease-fast=true
 
-cd benchmark
-
 echo 'Generating Benchmark Data...'
-source gen_data.sh
+zig_bench_path="./zig-out/bin/run_bench"
+datapath="benchmark/data/exec_time.json"
+${zig_bench_path} std_block_merge quick tim comb shell heap radix twin > ${datapath}
 echo 'Data Generated'
+
+cd benchmark
 
 python3 bars.py
 echo 'Created Benchmark Images in benchmark/image'
--- benchmark/run_bench.zig
+++ benchmark/run_bench.zig
@@ -1,61 +1,154 @@
 const zort = @import("zort");
 const std = @import("std");
-const mem = std.mem;
-const eql = std.mem.eql;
-const process = std.process;
 const testing = std.testing;
-const sort = std.sort;
 
-fn e(a: []const u8, b: []const u8) bool {
-    return eql(u8, a, b);
+const Str = []const u8;
+
+const INPUT_ITEMS = 10_000_000;
+const RUNS = 10;
+
+const BenchResult = struct {
+    command: Str,
+    mean: u64,
+    times: [RUNS]u64,
+};
+
+pub fn main() !void {
+    const args = try std.process.argsAlloc(testing.allocator);
+    defer std.process.argsFree(testing.allocator, args);
+
+    // generate random data
+    const rand_engine = std.rand.DefaultPrng.init(@intCast(u64, std.time.milliTimestamp())).random();
+    var arr = try std.ArrayList(usize).initCapacity(testing.allocator, INPUT_ITEMS);
+    defer arr.deinit();
+    std.debug.print("Generating random data ({d} items)... ", .{INPUT_ITEMS});
+    var i: usize = 0;
+    while (i < INPUT_ITEMS) : (i += 1) {
+        arr.appendAssumeCapacity(rand_engine.intRangeAtMostBiased(usize, 0, INPUT_ITEMS));
+    }
+    std.debug.print("Done. ", .{});
+
+    var results = std.ArrayList(BenchResult).init(std.testing.allocator);
+    defer results.deinit();
+
+    for (args[1..]) |arg| {
+        std.debug.print("\nStarting {s} sort...", .{arg});
+        var result: BenchResult = undefined;
+
+        i = 0;
+        while (i < RUNS) : (i += 1) {
+            var items = try testing.allocator.dupe(usize, arr.items);
+
+            if (std.mem.eql(u8, arg, "bubble"))
+                result.times[i] = try bench(zort.bubbleSort, .{ usize, items, asc })
+            else if (std.mem.eql(u8, arg, "quick"))
+                result.times[i] = try bench(zort.quickSort, .{ usize, items, asc })
+            else if (std.mem.eql(u8, arg, "insertion"))
+                result.times[i] = try bench(zort.insertionSort, .{ usize, items, asc })
+            else if (std.mem.eql(u8, arg, "selection"))
+                result.times[i] = try bench(zort.selectionSort, .{ usize, items, asc })
+            else if (std.mem.eql(u8, arg, "comb"))
+                result.times[i] = try bench(zort.combSort, .{ usize, items, asc })
+            else if (std.mem.eql(u8, arg, "shell"))
+                result.times[i] = try bench(zort.shellSort, .{ usize, items, asc })
+            else if (std.mem.eql(u8, arg, "heap"))
+                result.times[i] = try bench(zort.heapSort, .{ usize, items, asc })
+            else if (std.mem.eql(u8, arg, "merge"))
+                result.times[i] = try errbench(
+                    zort.mergeSort,
+                    .{ usize, items, asc, testing.allocator },
+                )
+            else if (std.mem.eql(u8, arg, "radix"))
+                result.times[i] = try errbench(
+                    zort.radixSort,
+                    .{ usize, items, testing.allocator },
+                )
+            else if (std.mem.eql(u8, arg, "tim"))
+                result.times[i] = try errbench(
+                    zort.timSort,
+                    .{ usize, items, asc, testing.allocator },
+                )
+            else if (std.mem.eql(u8, arg, "twin"))
+                result.times[i] = try errbench(
+                    zort.twinSort,
+                    .{ std.testing.allocator, usize, items, {}, comptime std.sort.asc(usize) },
+                )
+            else if (std.mem.eql(u8, arg, "std_block_merge"))
+                result.times[i] = try bench(
+                    std.sort.sort,
+                    .{ usize, items, {}, comptime std.sort.asc(usize) },
+                )
+            else if (std.mem.eql(u8, arg, "std_insertion"))
+                result.times[i] = try bench(
+                    std.sort.insertionSort,
+                    .{ usize, items, {}, comptime std.sort.asc(usize) },
+                )
+            else
+                std.debug.panic("{s} is not a valid argument", .{arg});
+
+            std.debug.print("\r{s:<20} round: {d:>2}/{d:<10} time: {d} ms", .{ arg, i + 1, RUNS, result.times[i] });
+        }
+
+        var sum: usize = 0;
+        for (result.times) |time| {
+            sum += time;
+        }
+        result.command = try std.fmt.allocPrint(testing.allocator, "{s} {s}", .{ args[0], arg });
+        result.mean = sum / RUNS;
+
+        try results.append(result);
+    }
+
+    try writeResults(results);
 }
 
 fn asc(a: usize, b: usize) bool {
     return a < b;
 }
 
-pub fn main() !void {
-    const args = try process.argsAlloc(testing.allocator);
-    defer process.argsFree(testing.allocator, args);
+fn bench(func: anytype, args: anytype) anyerror!u64 {
+    var timer = try std.time.Timer.start();
+    @call(.{}, func, args);
+    return timer.read() / std.time.ns_per_ms;
+}
 
-    const rand_engine = std.rand.DefaultPrng.init(@intCast(u64, std.time.milliTimestamp())).random();
-    var arr = std.ArrayList(usize).init(testing.allocator);
-    defer arr.deinit();
-    var i: usize = 0;
-    // 10 ^ 7
-    while (i < 10_000_000) : (i += 1) {
-        try arr.append(rand_engine.intRangeAtMostBiased(usize, 0, 10000000));
-    }
-    var items = try testing.allocator.dupe(usize, arr.items);
+fn errbench(func: anytype, args: anytype) anyerror!u64 {
+    var timer = try std.time.Timer.start();
+    try @call(.{}, func, args);
+    return timer.read() / std.time.ns_per_ms;
+}
 
-    for (args[1..]) |arg| {
-        if (e(arg, "bubble"))
-            zort.bubbleSort(usize, items, asc)
-        else if (e(arg, "quick"))
-            zort.quickSort(usize, items, asc)
-        else if (e(arg, "insertion"))
-            zort.insertionSort(usize, items, asc)
-        else if (e(arg, "selection"))
-            zort.selectionSort(usize, items, asc)
-        else if (e(arg, "comb"))
-            zort.combSort(usize, items, asc)
-        else if (e(arg, "shell"))
-            zort.shellSort(usize, items, asc)
-        else if (e(arg, "heap"))
-            zort.heapSort(usize, items, asc)
-        else if (e(arg, "merge"))
-            try zort.mergeSort(usize, items, asc, testing.allocator)
-        else if (e(arg, "radix"))
-            try zort.radixSort(usize, items, testing.allocator)
-        else if (e(arg, "tim"))
-            try zort.timSort(usize, items, asc, testing.allocator)
-        else if (e(arg, "std_block_merge"))
-            sort.sort(usize, items, {}, comptime sort.asc(usize))
-        else if (e(arg, "std_insertion"))
-            sort.insertionSort(usize, items, {}, comptime sort.asc(usize))
-        else if (e(arg, "twin"))
-            try zort.twinSort(testing.allocator, usize, items, {}, comptime sort.asc(usize))
-        else
-            std.debug.panic("{s} is not a valid argument", .{arg});
+fn writeResults(results: std.ArrayList(BenchResult)) !void {
+    const stdout = std.io.getStdOut().writer();
+
+    var w = std.json.writeStream(stdout, 10);
+
+    try w.beginObject();
+    try w.objectField("results");
+    try w.beginArray();
+
+    for (results.items) |res| {
+        try w.arrayElem();
+        try w.beginObject();
+
+        try w.objectField("command");
+        try w.emitString(res.command);
+
+        try w.objectField("mean");
+        try w.emitNumber(res.mean);
+
+        try w.objectField("times");
+        try w.beginArray();
+        for (res.times) |time| {
+            try w.arrayElem();
+            try w.emitNumber(time);
+        }
+        try w.endArray();
+
+        try w.endObject();
     }
+
+    try w.endArray();
+
+    try w.endObject();
 }
