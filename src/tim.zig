alichraghi/zort

commit 3200a76a828c00c1888e9ea9d1d4ec31aad8ff45

Quick fix for new compiler 0.11.0-dev.1247+87b223428

src/tim.zig
@@ -589,8 +589,9 @@ fn TimSort(
 
                 while (offset < max_offset and self.cmp(context, items[base + hint + offset], key)) {
                     last_offset = offset;
-                    var res: usize = undefined;
-                    if (@shlWithOverflow(usize, offset, 1, &res)) {
+                    var res: usize = undefined; 
+                    const ov = @shlWithOverflow(offset, @intCast(u6, 1));
+                    if (ov[1] == 1) {
                         offset = max_offset;
                     } else {
                         offset = res +| 1;
@@ -608,7 +609,8 @@ fn TimSort(
                 while (offset < max_offset and !self.cmp(context, items[base + hint - offset], key)) {
                     last_offset = offset;
                     var res: usize = undefined;
-                    if (@shlWithOverflow(usize, offset, 1, &res)) {
+                    const ov = @shlWithOverflow(offset, @intCast(u6, 1));
+                    if (ov[1] == 1) {
                         offset = max_offset;
                     } else {
                         offset = res +| 1;
@@ -659,7 +661,8 @@ fn TimSort(
                 while (offset < max_offset and self.cmp(context, key, items[base + hint - offset])) {
                     last_offset = offset;
                     var res: usize = undefined;
-                    if (@shlWithOverflow(usize, offset, 1, &res)) {
+                    const ov = @shlWithOverflow(offset, @intCast(u6, 1));
+                    if (ov[1] == 1) {
                         offset = max_offset;
                     } else {
                         offset = res +| 1;
@@ -678,7 +681,8 @@ fn TimSort(
                 while (offset < max_offset and !self.cmp(context, key, items[base + hint + offset])) {
                     last_offset = offset;
                     var res: usize = undefined;
-                    if (@shlWithOverflow(usize, offset, 1, &res)) {
+                    const ov = @shlWithOverflow(offset, @intCast(u6, 1));
+                    if (ov[1] == 1) {
                         offset = max_offset;
                     } else {
                         offset = res +| 1;

benchmark/run_bench.zig
@@ -45,8 +45,7 @@ pub fn main() !void {
                 "Generating {s} data ({d} items of {s})... ",
                 .{ flavor_name, INPUT_ITEMS, @typeName(tp) },
             );
-            const opts = std.builtin.CallOptions{};
-            const arr = try @call(opts, flavor, .{ tp, allocator, INPUT_ITEMS });
+            const arr = try @call(.auto, flavor, .{ tp, allocator, INPUT_ITEMS });
             defer allocator.free(arr);
             std.debug.print("Done. ", .{});
 
@@ -172,13 +171,13 @@ fn runIterations(
 
 fn bench(func: anytype, args: anytype) anyerror!u64 {
     var timer = try std.time.Timer.start();
-    @call(.{}, func, args);
+    @call(.auto, func, args);
     return timer.read() / std.time.ns_per_ms;
 }
 
 fn errbench(func: anytype, args: anytype) anyerror!u64 {
     var timer = try std.time.Timer.start();
-    try @call(.{}, func, args);
+    try @call(.auto, func, args);
     return timer.read() / std.time.ns_per_ms;
 }
 

src/radix.zig
@@ -79,7 +79,7 @@ inline fn insSortIntoOtherArray(comptime T: type, comptime options: SortOptions,
 
 inline fn truncate(comptime U: type, x: anytype) U {
     const T = @TypeOf(x);
-    if (std.meta.bitCount(T) >= std.meta.bitCount(U)) {
+    if (@bitSizeOf(T) >= @bitSizeOf(U)) {
         return @truncate(U, x);
     }
     return x;
@@ -96,7 +96,7 @@ inline fn readOneByte(comptime T: type, comptime options: SortOptions, comptime 
     if (T == f64) {
         return readOneByte(u64, .{}, idx, @bitCast(u64, x));
     }
-    const U = comptime std.meta.Int(.unsigned, std.meta.bitCount(T));
+    const U = comptime std.meta.Int(.unsigned, @bitSizeOf(T));
     const shift = comptime (@sizeOf(T) - 1 - idx) * 8;
     if (idx == 0) {
         return truncate(u8, (@bitCast(U, @as(T, std.math.minInt(T))) ^ @bitCast(U, x)) >> shift);
@@ -115,7 +115,7 @@ inline fn readTwoBytes(comptime T: type, comptime options: SortOptions, comptime
     if (T == f64) {
         return readTwoBytes(u64, .{}, idx, @bitCast(u64, x));
     }
-    const U = comptime std.meta.Int(.unsigned, std.meta.bitCount(T));
+    const U = comptime std.meta.Int(.unsigned, @bitSizeOf(T));
     const shift = comptime (@sizeOf(T) - 2 - idx) * 8;
     if (idx == 0) {
         return truncate(u16, (@bitCast(U, @as(T, std.math.minInt(T))) ^ @bitCast(U, x)) >> shift);
@@ -362,14 +362,14 @@ pub fn radixSort(
     // the max number of buckets needed is for the case where we consume
     // 2 bytes at a time the whole way through.
     // In that case, we'll need 0x10000 * (n_levels+1) buckets.
-    const max_buckets = (((std.meta.bitCount(Key) + 15) / 16) + 1) * 0x10000;
+    const max_buckets = (((@bitSizeOf(Key) + 15) / 16) + 1) * 0x10000;
 
     // using usize for buckets is pretty wasteful - most arrays have <2^32 elements.
     // this has a quite real performance cost because fewer buckets fit in cache.
     // so let's have a u32 version?
     const scratch = try allocator.alloc(T, arr.len);
     defer allocator.free(scratch);
-    if (arr.len <= std.math.maxInt(u32) and std.meta.bitCount(usize) > 32) {
+    if (arr.len <= std.math.maxInt(u32) and @bitSizeOf(usize) > 32) {
         const buckets = try allocator.alloc(u32, max_buckets);
         defer allocator.free(buckets);
         if (@sizeOf(T) > 1 and arr.len >= 0x10000) {

commit 6eba27e335e7f1f254acb80efc5e32a60ce5a3a8

TimSort: fix gallopLeft and gallopRight
3200a76a82 introduced a bug while fixing a compilation error:
        last_offset = offset;
-       var res: usize = undefined;
-       if (@shlWithOverflow(usize, offset, 1, &res)) {
+       var res: usize = undefined;
+       const ov = @shlWithOverflow(offset, @intCast(u6, 1));
+       if (ov[1] == 1) {
            offset = max_offset;
        } else {
            offset = res +| 1;
- the `res` variable should have been removed as new @shlWithOverflow
does not touch it anymore
- last line should be `offset = ov[0] +| 1`, because `ov[0]` holds the
shifted value
- the `+|` can be just `+` as after shifting left the last bit is 0 it
can not overflow when we add 1, it just makes offset an odd number
- when the shift overflows we can just break out from the loop as
`offset < max_offset` can not be true after `offset = max_offset`

src/tim.zig
@@ -605,13 +605,11 @@ fn TimSort(
 
                 while (offset < max_offset and self.cmp(context, items[base + hint + offset], key)) {
                     last_offset = offset;
-                    const res: usize = undefined;
                     const ov = @shlWithOverflow(offset, @as(u6, 1));
                     if (ov[1] == 1) {
-                        offset = max_offset;
-                    } else {
-                        offset = res +| 1;
+                        break;
                     }
+                    offset = ov[0] + 1;
                 }
 
                 if (offset > max_offset) offset = max_offset;
@@ -624,13 +622,11 @@ fn TimSort(
                 const max_offset = hint + 1;
                 while (offset < max_offset and !self.cmp(context, items[base + hint - offset], key)) {
                     last_offset = offset;
-                    const res: usize = undefined;
                     const ov = @shlWithOverflow(offset, @as(u6, 1));
                     if (ov[1] == 1) {
-                        offset = max_offset;
-                    } else {
-                        offset = res +| 1;
+                        break;
                     }
+                    offset = ov[0] + 1;
                 }
 
                 if (offset > max_offset) offset = max_offset;
@@ -676,13 +672,11 @@ fn TimSort(
 
                 while (offset < max_offset and self.cmp(context, key, items[base + hint - offset])) {
                     last_offset = offset;
-                    const res: usize = undefined;
                     const ov = @shlWithOverflow(offset, @as(u6, 1));
                     if (ov[1] == 1) {
-                        offset = max_offset;
-                    } else {
-                        offset = res +| 1;
+                        break;
                     }
+                    offset = ov[0] + 1;
                 }
 
                 if (offset > max_offset) offset = max_offset;
@@ -696,13 +690,11 @@ fn TimSort(
                 const max_offset = len - hint;
                 while (offset < max_offset and !self.cmp(context, key, items[base + hint + offset])) {
                     last_offset = offset;
-                    const res: usize = undefined;
                     const ov = @shlWithOverflow(offset, @as(u6, 1));
                     if (ov[1] == 1) {
-                        offset = max_offset;
-                    } else {
-                        offset = res +| 1;
+                        break;
                     }
+                    offset = ov[0] + 1;
                 }
 
                 if (offset > max_offset) offset = max_offset;